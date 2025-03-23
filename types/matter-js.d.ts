af11864fd40d9fd5691ed8d417eb834bc0c3b02e
csprout3 <csprout3@stanford.edu>
2025-03-23 01:07:57 -0700

    Add fallback type declaration for matter-js

---
+++ types/matter-js.d.ts
@@ -0,0 +1,6 @@
+declare module 'matter-js' {
+  // This is a simplified declaration for matter-js
+  // If more specific types are needed, use the @types/matter-js package
+  const Matter: any;
+  export default Matter;
+} 
